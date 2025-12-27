0a61becd59f51174e02be66ada670a5dec0571b6
Sebastian H. <maracuchoamericano@gmail.com>
2025-12-27 17:02:52 -0500

    added or

--- Call.js
+++ Call.js
@@ -1126,7 +1126,7 @@ KNOWN_DATA: Name=${this.customerName || "?"}, Car=${this.vehicleModel || "?"}, E
                     t: this.appointmentTime
                 });
 
-                if (this.lastAttemptedDetails === currentAttempt && (this.hasScheduledAppointment || !this.hasConfirmedDetails)) {
+                if (this.lastAttemptedDetails === currentAttempt || (this.hasScheduledAppointment || !this.hasConfirmedDetails)) {
                     console.log(`[${this.callSid}] Skipping scheduling - details unchanged from last failure or success.`);
                 } else {
 
